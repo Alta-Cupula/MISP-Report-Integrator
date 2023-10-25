0fe2e8cade718177e3b88862551c9f12f9190630
JLowborn <64245567+JLowborn@users.noreply.github.com>
2023-10-25 16:07:26 -0300

    Delete code/tests directory

--- code/tests/hash_identifier.py
+++
@@ -1,9 +0,0 @@
-from hashid import HashID
-
-_hash = "3ce8b0ffd75bc240fc7d967729cd6637"
-
-identifier = HashID()
-
-results = identifier.identifyHash(_hash)
-
-print([type.name for type in results])
--- code/tests/pathfinder.py
+++
@@ -1,3 +0,0 @@
-from pathlib import Path
-
-print(Path(__name__).parent.absolute())
